a065aca098a633badb9f81eca5e24444f458bf28
npaessel <npaessel@f6965e7d-1634-43f8-88a7-533111520b2a>
2010-05-07 23:40:23 +0000

    add skel config files for hudson integration
    git-svn-id: https://svn.concord.org/svn/diy/trunk@245 f6965e7d-1634-43f8-88a7-533111520b2a

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -10,9 +10,6 @@ module NavigationHelpers
     
     when /the home\s?page/
       '/'
-    when /the new activity page/
-      new_activity_path
-
     
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
