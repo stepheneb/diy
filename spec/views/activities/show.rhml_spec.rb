f5e97030cab9076cc55529857256a1104d2bf44f
npaessel <npaessel@f6965e7d-1634-43f8-88a7-533111520b2a>
2010-05-11 15:27:44 +0000

    View testing is currently not working.
    git-svn-id: https://svn.concord.org/svn/diy/trunk@252 f6965e7d-1634-43f8-88a7-533111520b2a

---
+++ spec/views/activities/show.rhml_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "/activities/show.rhtml" do
+  # this generated block is not working, because there is something wrong with
+  # the current version mixture of rspec / rails / for the DIY project.
+  # TODO: Talk to aaron, others about upgrading?
+  #
+  # before(:each) do
+  #   assigns[:activity] = @activity = stub_model(Activity)
+  # end
+  # 
+  # it "renders attributes in <p>" do
+  #   render
+  # end
+end
