stepheneb/diy

commit 0ca16d6c2f8b03943d6bbe437ccaabe964eccae2

External OTML will use DIY configured vendor iface
git-svn-id: https://svn.concord.org/svn/diy/trunk@295 f6965e7d-1634-43f8-88a7-533111520b2a

app/helpers/otml_helper.rb
@@ -107,12 +107,16 @@ module OtmlHelper
           end
         }
       }
-      xml.OTInterfaceManager {
-        xml.deviceConfigs {
-          @vendor_interface.device_configs.each do |dc|
-            xml.OTDeviceConfig("configString" => dc.config_string, "deviceId" => @vendor_interface.device_id)
-          end
-        }
+      otml_device_config(xlm)
+    }
+  end
+
+  def otml_device_config(xml)
+    xml.OTInterfaceManager {
+      xml.deviceConfigs {
+        @vendor_interface.device_configs.each do |dc|
+          xml.OTDeviceConfig("configString" => dc.config_string, "deviceId" => @vendor_interface.device_id)
+        end
       }
     }
   end

app/controllers/external_otrunk_activities_controller.rb
@@ -182,6 +182,7 @@ class ExternalOtrunkActivitiesController < ApplicationController
 
   def overlay_otml
     @user = User.find(params[:uid])
+    find_vendor_interface
     activity = @external_otrunk_activity
     learner = activity.find_or_create_learner(@user)
     group_id = params[:group_id]
@@ -298,5 +299,14 @@ class ExternalOtrunkActivitiesController < ApplicationController
       end
     end
   end
+  
+  def find_vendor_interface
+    unless defined? @vendor_id
+      if (params[:vid])
+        @vendor_interface = VendorInterface.find(params[:vid])
+      end
+    end
+  end
+
   
 end

app/views/shared/overlay_otml.builder
@@ -11,7 +11,12 @@ xml.otrunk("xmlns:fo" => "http://www.w3.org/1999/XSL/Format", "xmlns:lxslt" => "
       }
       xml.bundles {
         @bundles.each do |r|
-          xml.object("refid" => r)
+          # use the DIY configured interface manager:
+          if (r.to_s =~/interface_manager/) 
+            otml_device_config(xml)
+          else
+            xml.object("refid" => r)
+          end
         end
         if @learners.size > 0 || @userListURL
           hash = {}

commit 20243059c634607f86e9cdd8ff3b638d686a24bf

fix typo introduced in commit 0ca16d6c2f8b
git-svn-id: https://svn.concord.org/svn/diy/trunk@299 f6965e7d-1634-43f8-88a7-533111520b2a

app/helpers/otml_helper.rb
@@ -107,7 +107,7 @@ module OtmlHelper
           end
         }
       }
-      otml_device_config(xlm)
+      otml_device_config(xml)
     }
   end
 