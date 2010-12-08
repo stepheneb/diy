9a34accef7c0c0efb304588082ea4534bced384e
npaessel <npaessel@f6965e7d-1634-43f8-88a7-533111520b2a>
2010-12-08 15:25:12 +0000

    all apps use passenger
    git-svn-id: https://svn.concord.org/svn/diy/trunk@306 f6965e7d-1634-43f8-88a7-533111520b2a

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -18,11 +18,12 @@ set :local_production_database_prefix, "production_#{clean_app_name}"
 set :local_staging_database_prefix, "staging_#{clean_app_name}"
 
 set(:use_passenger) do
-  if ['itsisu_diy'].index(application)
+  # if ['itsisu_diy', 'itsi','capa'].index(application)
+  # NP: everything does now
     true
-  else
+  # else
     # Capistrano::CLI.ui.agree( "Use passenger for deployment? (y/n) ", false)
     # We could ask... but nothing else really uses passenger I think..
-    false 
+  # false
   end
 end
