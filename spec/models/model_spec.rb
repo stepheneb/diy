b448c89eccb0130d21048846604a0bb9e639188e
npaessel <npaessel@f6965e7d-1634-43f8-88a7-533111520b2a>
2011-04-01 18:32:25 +0000

    celan whitespace
    git-svn-id: https://svn.concord.org/svn/diy/trunk@322 f6965e7d-1634-43f8-88a7-533111520b2a

--- spec/models/model_spec.rb
+++ spec/models/model_spec.rb
@@ -10,23 +10,23 @@ describe Model do
   it "should create a new instance given valid attributes" do
     Model.create!(@valid_attributes)
   end
-  
+
   describe "should let authors attach images" do
     before(:each) do
       @model = Model.create!(@valid_attributes)
     end
-    
+
     it "should have image methods" do
       @model.should respond_to(:image_url)
       @model.should respond_to(:image_url=)
       @model.should respond_to(:has_image?)
     end
-    
+
     it "should not have an image at first" do
       @model.has_image?.should be false
       @model.image_url.should be nil
     end
-    
+
     it "should accept valid image urls" do
       @model.image_url="http://www.concord.org/images/logos/cc/cc_main_banner.jpg"
       @model.save.should be true
@@ -34,7 +34,7 @@ describe Model do
       @model.image_url.should == "http://www.concord.org/images/logos/cc/cc_main_banner.jpg"
       @model.has_image?.should be true
     end
-    
+
     it "should reject invalid image urls" do
       @model.image_url="http://www.concord.org/images/logos/cc/cc_main_banner.foo"
       @model.save.should be false
